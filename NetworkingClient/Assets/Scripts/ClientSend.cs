f5d6ee57a51246a99156a8ef4a374d1fe44231ea
wonderkatt <55135528+wonderkatt@users.noreply.github.com>
2020-02-17 18:19:20 +0100

    part3

---
+++ NetworkingClient/Assets/Scripts/ClientSend.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientSend : MonoBehaviour
+{
+    private static void SendTCPData(Packet packet)
+    {
+        packet.WriteLength();
+        Client.Instance.tcp.SendData(packet);
+    }
+
+    private static void SendUDPData(Packet packet)
+    {
+        packet.WriteLength();
+        Client.Instance.udp.SendData(packet);
+    }
+        
+
+    #region
+    public static void WelcomeRecieved()
+    {
+        using(Packet packet = new Packet((int)ClientPackets.welcomeReceived))
+        {
+            packet.Write(Client.Instance.MyId);
+            packet.Write(UIManager.Instance.UserNameField.text);
+
+            SendTCPData(packet);
+        }
+    }
+
+    internal static void UdpTestRecieve()
+    {
+        using (Packet packet = new Packet((int)ClientPackets.udpTestRecieve))
+        {
+            packet.Write("Recieved a UDP packet");
+            SendUDPData(packet);
+        }
+    }
+    #endregion
+}
